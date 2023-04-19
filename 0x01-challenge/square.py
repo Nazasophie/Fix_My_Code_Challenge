65f37f9ad665b8844a6a907b165b82f8f65c9f99
Nazasophie <chinazasundaygoodness@gmail.com>
2023-04-19 05:12:26 -0700

    Fix_My_Code_Challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,49 +1,33 @@
 #!/usr/bin/python3
+""" Module for square class"""
 
-class Square:
-    
+
+class Square():
+    """ Square class """
+    width = 0
+    height = 0
+
     def __init__(self, *args, **kwargs):
-        """
-        Initialize the Square object with the given width and height.
-
-        Args:
-        - width (int): the width of the square.
-        - height (int): the height of the square.
-        """
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """
-        Calculate the area of the square.
+        """ Area of the square """
+        return self.width * self.height
 
-        Returns:
-        - int: the area of the square.
-        """
-        return self.width * self.width
-
-    def perimeter_of_my_square(self):
-        """
-        Calculate the perimeter of the square.
-
-        Returns:
-        - int: the perimeter of the square.
-        """
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """
-        Return a string representation of the square.
-
-        Returns:
-        - str: a string representation of the square.
-        """
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """ Create a square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
-
+    print(s.permiter_of_my_square())
